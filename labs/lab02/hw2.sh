742e7d51ca0a42f5d5b5e0ee852663348938afb5
askar12345678 <askarval86@gmail.com>
2022-10-15 12:52:44 +0300

    Modified 1 and 2 labs

--- labs/lab02/hw2.sh
+++ labs/lab02/hw2.sh
@@ -9,7 +9,7 @@ echo "os: $OSTYPE" >> info.txt
 echo -n "home directory size: " >> info.txt
 du -sh --apparent-size | awk '{print $1}' >> info.txt
 echo -n "free space on disk: " >> info.txt
-df . -h /tmp | tail -1 | awk '{print $4}' >> info.txt
+df . -h | tail -1 | awk '{print $4}' >> info.txt
 echo -n "num folders: " >> info.txt
 ls -l | grep ^d | wc -l >> info.txt
 echo -n "num files: $x" >> info.txt
